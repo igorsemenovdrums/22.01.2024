8ec63e73b652c29f118008ac62f25531ece5deb3
igorsemenovdrums <igorsemenov@gmx.com>
2024-01-23 21:46:01 +0200

    Lesson 2

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1st solution
+
+const c = +prompt("Celsius");
+const f = (c * (9/5) + 32);
+console.log(f + " Fahrenheit");
+
+// 2nd solution
+
+const a = +prompt("C");
+const b = (a * 1.8 + 32);
+alert(b + " F");
+
+// 3rd solution
+
+const x = +prompt("Celsius degrees");
+console.log(`${x*1.8+32} degrees of Fahrenheit`)
